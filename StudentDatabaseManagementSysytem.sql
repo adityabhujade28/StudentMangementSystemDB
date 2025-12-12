cce01b1ec5a829ef8175160a1be01424b943f3f0
adityabhujade28 <aditya.bhujade@kratin.co.in>
2025-12-12 10:26:11 +0530

    Removed Enrollment date column from student course table

--- StudentDatabaseManagementSysytem.sql
+++ StudentDatabaseManagementSysytem.sql
@@ -39,8 +39,8 @@ CREATE TABLE Course (
 CREATE TABLE StudentCourse (
     StudentId INT NOT NULL,
     CourseId INT NOT NULL,
-    EnrollmentDate DATE DEFAULT CAST(GETDATE() AS DATE),
 
+    
     CONSTRAINT PK_StudentCourse PRIMARY KEY (StudentId, CourseId),
 
     CONSTRAINT FK_StudentCourse_Student FOREIGN KEY (StudentId)
